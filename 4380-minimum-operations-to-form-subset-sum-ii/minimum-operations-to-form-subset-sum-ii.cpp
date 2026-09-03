3c9ec4b2e2a537b164c63addc7ef79e88df479ca
Shivam Chauhan <63206281+doitwithcod3@users.noreply.github.com>
2026-09-03 19:46:41 +0530

    Time: 1959 ms (5.91%) | Memory: 72.9 MB (40.63%) - LeetSync

---
+++ 4380-minimum-operations-to-form-subset-sum-ii/minimum-operations-to-form-subset-sum-ii.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    const int inf = 1e9;
+    
+    void dfs(int current, int maxval, int iteration, map<int, int> &res) {
+        if (0 >= current) return;
+        if (current > maxval) return dfs(current / 2, maxval, iteration + 1, res); 
+        
+        if (res.find(current) != res.end()) {
+            if (res[current] < iteration) return;
+        }
+        res[current] = iteration;
+        dfs(current / 2, maxval, iteration + 1, res);
+        dfs(current * 2, maxval, iteration + 1, res);
+    }
+
+    map<int, int> findall(int num, int maxval) {
+        map<int, int> res;
+        dfs(num, maxval, 0, res);
+        return res;
+    }
+
+    int minOperations(vector<int>& nums, int sum) {
+        int n = nums.size();
+        vector dp(n + 1, vector<int> (sum + 1, inf));
+        dp[0][0] = 0;
+        for (int index = 1; index <= n; ++index) {
+            dp[index][0] = 0;
+            for (auto it : findall(nums[index - 1], sum)) {
+                int num = it.first, val = it.second;
+                cout << num << " " << val << endl;
+                for (int s = 1; s <= sum; ++s) {
+                    if (0 <= s - num) dp[index][s] = min(dp[index][s], dp[index - 1][s - num] + val);
+                    dp[index][s] = min(dp[index][s], dp[index - 1][s]);
+                }
+            }
+            cout << endl;
+        }
+        return dp[n][sum] >= inf ? -1 : dp[n][sum];
+    }
+};
