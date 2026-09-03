b3b349c12b741ad9952865f651cdecae675a1e8c
Shivam Chauhan <63206281+doitwithcod3@users.noreply.github.com>
2026-09-03 20:09:59 +0530

    Time: 661 ms (61.75%) | Memory: 73.1 MB (40.57%) - LeetSync

--- 4380-minimum-operations-to-form-subset-sum-ii/minimum-operations-to-form-subset-sum-ii.cpp
+++ 4380-minimum-operations-to-form-subset-sum-ii/minimum-operations-to-form-subset-sum-ii.cpp
@@ -25,16 +25,13 @@ public:
         vector dp(n + 1, vector<int> (sum + 1, inf));
         dp[0][0] = 0;
         for (int index = 1; index <= n; ++index) {
-            dp[index][0] = 0;
+            dp[index] = dp[index - 1];
             for (auto it : findall(nums[index - 1], sum)) {
                 int num = it.first, val = it.second;
-                cout << num << " " << val << endl;
                 for (int s = 1; s <= sum; ++s) {
                     if (0 <= s - num) dp[index][s] = min(dp[index][s], dp[index - 1][s - num] + val);
-                    dp[index][s] = min(dp[index][s], dp[index - 1][s]);
                 }
             }
-            cout << endl;
         }
         return dp[n][sum] >= inf ? -1 : dp[n][sum];
     }
