95e1cc5f5a7c4985ed321150434c576b813b62d3
Shivam Chauhan <63206281+doitwithcod3@users.noreply.github.com>
2026-09-06 15:19:37 +0530

    Time: 23 ms (77.66%) | Memory: 27 MB (52.57%) - LeetSync

--- Leetcode/115-distinct-subsequences/distinct-subsequences.cpp
+++ Leetcode/115-distinct-subsequences/distinct-subsequences.cpp
@@ -5,11 +5,10 @@ public:
     int numDistinct(string s, string t) {
         int n = s.size(), m = t.size();
         vector<vector<int>> dp(n + 1, vector<int> (m + 1, 0));
-        // there is one way to get match when target = ""
-        // irrespective of the length of the subsequence. I wont take any char, so count = 1
-        for (int i = 0; i <= n; ++i) dp[i][0] = 1; 
-        
+        dp[0][0] = 1;
+
         for (int i = 1; i <= n; ++i) {
+            dp[i][0] = 1;
             for (int j = 1; j <= m; ++j) {
                 dp[i][j] = (dp[i - 1][j] + 0LL + (s[i - 1] == t[j - 1] ? dp[i - 1][j - 1] : 0)) % mod;
             }
