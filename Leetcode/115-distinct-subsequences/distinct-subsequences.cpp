82b968f930d74b85d5cd73bced6f000a8cf897af
Shivam Chauhan <63206281+doitwithcod3@users.noreply.github.com>
2026-09-06 15:21:52 +0530

    Time: 15 ms (82.67%) | Memory: 8.8 MB (98.28%) - LeetSync

--- Leetcode/115-distinct-subsequences/distinct-subsequences.cpp
+++ Leetcode/115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     const int mod = 1e9 + 7;
-    // Iterative
+    // Iterative + Space Optimized
     int numDistinct(string s, string t) {
         int n = s.size(), m = t.size();
-        vector<vector<int>> dp(n + 1, vector<int> (m + 1, 0));
-        dp[0][0] = 1;
+        vector<int> one(m + 1, 0);
+        vector<int> two(m + 1, 0);
+        one[0] = 1;
 
         for (int i = 1; i <= n; ++i) {
-            dp[i][0] = 1;
+            two[0] = 1;
             for (int j = 1; j <= m; ++j) {
-                dp[i][j] = (dp[i - 1][j] + 0LL + (s[i - 1] == t[j - 1] ? dp[i - 1][j - 1] : 0)) % mod;
+                two[j] = (one[j] + 0LL + (s[i - 1] == t[j - 1] ? one[j - 1] : 0)) % mod;
             }
+            one = two;
         }
-        return dp[n][m];
+        return two[m];
     }
 };
