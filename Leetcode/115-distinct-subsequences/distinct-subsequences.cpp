fd58218d3116dae3a953bd1f11fe8962c334c4c8
Shivam Chauhan <63206281+doitwithcod3@users.noreply.github.com>
2026-09-06 15:18:20 +0530

    Time: 25 ms (73.87%) | Memory: 27.1 MB (52.57%) - LeetSync

--- Leetcode/115-distinct-subsequences/distinct-subsequences.cpp
+++ Leetcode/115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
-    // Recursive Solution
-    int dfs(int i, int j, string& s, string& t, vector<vector<int>> &dp) {
-        if (j == t.size()) return 1;
-        if (i == s.size()) return 0;
-        if (dp[i][j] != -1) return dp[i][j];
-        
-        int ans = 0;
-        if (s[i] == t[j]) ans += dfs(i + 1, j + 1, s, t, dp);
-        ans += dfs(i + 1, j, s, t, dp);
-        return dp[i][j] = ans;
-    }
-
+    const int mod = 1e9 + 7;
+    // Iterative
     int numDistinct(string s, string t) {
         int n = s.size(), m = t.size();
-        vector<vector<int>> dp(n, vector<int> (m, -1));
-        return dfs(0, 0, s, t, dp);
+        vector<vector<int>> dp(n + 1, vector<int> (m + 1, 0));
+        // there is one way to get match when target = ""
+        // irrespective of the length of the subsequence. I wont take any char, so count = 1
+        for (int i = 0; i <= n; ++i) dp[i][0] = 1; 
+        
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= m; ++j) {
+                dp[i][j] = (dp[i - 1][j] + 0LL + (s[i - 1] == t[j - 1] ? dp[i - 1][j - 1] : 0)) % mod;
+            }
+        }
+        return dp[n][m];
     }
 };
